cd0a290cbf3c5405653cb7bdddd6055904f855e4
thyeone <thyeonee@gmail.com>
2024-12-15 04:04:50 +0900

    디자인 수정

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Text } from '@chakra-ui/react';
 import dayjs from 'dayjs';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -9,14 +9,17 @@ type PostProps = {
 
 export default function Post({ post }: PostProps) {
   const { url, title, description, date, thumbnail } = post;
+
   return (
     <Link href={url} className='flex phone:flex-row group gap-24 py-24 flex-col-reverse phone:items-center w-full'>
       <div className='flex flex-col flex-1'>
         <span className='text-2xl font-bold group-hover:text-teal-600 transition'>{title}</span>
-        <span className='mt-8 text-sm phone:text-15 phone:leading-24 text-gray-500'>{description}</span>
+        <Text fontSize='sm' className='mt-8 '>
+          {description}
+        </Text>
         <span className='mt-16 text-xs text-gray-500'>{dayjs(date).format('YYYY년 MM월 DD일')}</span>
       </div>
-      <div className='relative rounded-10 phone:rounded-12 shrink-0 w-full h-200 phone:w-130 phone:h-90 overflow-hidden'>
+      <div className='relative rounded-10 phone:rounded-12 shrink-0 w-full h-180 phone:w-130 phone:h-90 overflow-hidden'>
         {thumbnail && (
           <Image
             src={thumbnail}
