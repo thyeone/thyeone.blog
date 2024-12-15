b49759cd78a1ce4930acbe220ff198e6f07a3630
thyeone <thyeonee@gmail.com>
2024-12-15 20:03:40 +0900

    callout 컴포넌트 추가

---
+++ src/components/Callout.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import { Box, Flex, Icon, Stack, Text } from '@chakra-ui/react';
+import { CircleAlertIcon, CircleCheckBigIcon, InfoIcon } from 'lucide-react';
+import { title } from 'process';
+import React from 'react';
+import { IoWarningOutline } from 'react-icons/io5';
+import { MdCheckCircleOutline } from 'react-icons/md';
+
+type CalloutProps = {
+  type: 'info' | 'warning' | 'success';
+  title: string;
+  children: React.ReactNode;
+};
+
+const iconMap = {
+  info: InfoIcon,
+  warning: IoWarningOutline,
+  success: CircleCheckBigIcon,
+};
+
+const colorMap = {
+  info: {
+    bg: 'blue.100',
+    border: 'blue.400',
+    icon: 'blue.400',
+    title: 'blue.400',
+    content: 'gray.800',
+  },
+  warning: {
+    bg: 'red.100',
+    border: 'red.500',
+    icon: 'red.500',
+    title: 'red.500',
+    content: 'gray.800',
+  },
+  success: {
+    bg: 'green.50',
+    border: 'green.200',
+    icon: 'green.500',
+    title: 'green.500',
+    content: 'gray.800',
+  },
+};
+
+export default function Callout({ type, title, children }: CalloutProps) {
+  const IconComponent = iconMap[type];
+  const colors = colorMap[type];
+
+  return (
+    <Stack bg={colors.bg} borderLeft={`5px solid`} borderColor={colors.border} borderRadius='md' p='4' display='flex'>
+      <Flex align='center' gap={1}>
+        <Flex justify='center' align='center' bg={colors.bg}>
+          <Icon as={IconComponent} color={colors.icon} boxSize='20px' />
+        </Flex>
+        <Text m='0px !important' color={colors.title} fontSize='18px !important' fontWeight='bold !important'>
+          {title}
+        </Text>
+      </Flex>
+      <Box color={colors.content}>{children}</Box>
+    </Stack>
+  );
+}
--- src/components/PostBody.tsx
+++ src/components/PostBody.tsx
@@ -1,12 +1,18 @@
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import Prose from './Prose';
 import rehypePrettyCode from 'rehype-pretty-code';
+import Callout from './Callout';
+
+const components = {
+  Callout,
+};
 
 export default function PostBodyMDX({ content }: { content: Post['content'] }) {
   return (
     <Prose>
       <MDXRemote
         source={content}
+        components={components}
         options={{
           mdxOptions: {
             rehypePlugins: [[rehypePrettyCode]],
