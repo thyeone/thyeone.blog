a1a51b54ba7d8d74f95b6807fee9c96c40d699e7
thyeone <thyeonee@gmail.com>
2025-02-03 12:15:13 +0900

    MDXLink로 external 링크 적용

--- src/components/MDXLink.tsx
+++ src/components/MDXLink.tsx
@@ -1,15 +1,18 @@
-'use client';
+"use client";
 
-import { Link as NextLink } from '@chakra-ui/next-js';
-import { type AnchorHTMLAttributes } from 'react';
+import { Link as NextLink } from "@chakra-ui/next-js";
+import { type AnchorHTMLAttributes } from "react";
 
-const MDXLink = ({ href, ...props }: AnchorHTMLAttributes<HTMLAnchorElement>) => {
-  const isInternalLink = href && href.startsWith('/');
-  const isMDXLink = href && href.startsWith('20');
-  const isAnchorLink = href && href.startsWith('#');
+const MDXLink = ({
+  href,
+  ...props
+}: AnchorHTMLAttributes<HTMLAnchorElement>) => {
+  const isInternalLink = href && href.startsWith("/");
+  const isMDXLink = href && href.startsWith("20");
+  const isAnchorLink = href && href.startsWith("#");
 
   if (isInternalLink || isMDXLink) {
-    return <NextLink href={href} {...props} />;
+    return <NextLink target="_blank" href={href} {...props} />;
   }
 
   if (isAnchorLink) {
@@ -18,16 +21,16 @@ const MDXLink = ({ href, ...props }: AnchorHTMLAttributes<HTMLAnchorElement>) =>
         {...props}
         href={href}
         style={{
-          color: 'inherit',
-          textDecoration: 'none',
-          fontWeight: 'inherit',
+          color: "inherit",
+          textDecoration: "none",
+          fontWeight: "inherit",
         }}
-        className='anchor'
+        className="anchor"
       />
     );
   }
 
-  return <a target='_blank' rel='noopener noreferrer' href={href} {...props} />;
+  return <a target="_blank" rel="noopener noreferrer" href={href} {...props} />;
 };
 
 export default MDXLink;
