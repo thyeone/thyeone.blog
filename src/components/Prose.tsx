2a0f723acd5e00df4fa6a8a4e7c5e5dd779c45c5
thyeone <thyeonee@gmail.com>
2025-02-03 14:21:03 +0900

    style: blockquote 스타일 수정

--- src/components/Prose.tsx
+++ src/components/Prose.tsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
-import { Prose as ChakraProse } from '@nikolovlazar/chakra-ui-prose';
-import { useEffect, type PropsWithChildren } from 'react';
+import { Prose as ChakraProse } from "@nikolovlazar/chakra-ui-prose";
+import { useEffect, type PropsWithChildren } from "react";
 
 const Prose = ({ children }: PropsWithChildren) => {
   useEffect(() => {
-    const hash = window.decodeURI(location.hash.replace('#', ''));
-    if (hash !== '') {
+    const hash = window.decodeURI(location.hash.replace("#", ""));
+    if (hash !== "") {
       const element = document.getElementById(hash);
       if (element) {
         const offset = element.offsetTop;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,19 +1,19 @@
-import { extendTheme, type ThemeConfig } from '@chakra-ui/react';
-import { withProse } from '@nikolovlazar/chakra-ui-prose';
+import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
+import { withProse } from "@nikolovlazar/chakra-ui-prose";
 
 const config: ThemeConfig = {
-  initialColorMode: 'dark',
+  initialColorMode: "dark",
   useSystemColorMode: true,
 };
 
 const components = {
   Link: {
     baseStyle: {
-      color: 'blue.600',
-      fontWeight: 'bold',
-      textDecoration: 'underline',
+      color: "blue.600",
+      fontWeight: "bold",
+      textDecoration: "underline",
       _hover: {
-        color: 'blue.800',
+        color: "blue.800",
       },
     },
   },
@@ -26,7 +26,7 @@ export const theme = extendTheme(
     styles: {
       global: (props: any) => ({
         body: {
-          bg: props.colorMode === 'dark' ? '#0A0A0A' : '#FFFFFF', // 다크모드일 때의 배경색을 여기서 설정
+          bg: props.colorMode === "dark" ? "#0A0A0A" : "#FFFFFF", // 다크모드일 때의 배경색을 여기서 설정
         },
       }),
     },
@@ -34,10 +34,14 @@ export const theme = extendTheme(
   withProse({
     baseStyle: (props: any) => ({
       a: {
-        color: props.colorMode === 'dark' ? 'blue.400' : 'blue.500',
-        textDecoration: 'underline',
-        textUnderlineOffset: '3px',
-        textDecorationThickness: '0.5px',
+        color: props.colorMode === "dark" ? "blue.400" : "blue.500",
+        textDecoration: "underline",
+        textUnderlineOffset: "3px",
+        textDecorationThickness: "0.5px",
+      },
+      blockQuote: {
+        fontStyle: "inherit",
+        color: "gray.500",
       },
     }),
   })
